b929f1ad3bb55895ac3605418fa947c4244762c6
NDRAEY <pikachu_andrey@vk.com>
2024-07-29 15:18:14 +0900

    C++: Stabilize <map>'s back()

--- libcpp/maptest.cpp
+++ libcpp/maptest.cpp
@@ -9,7 +9,7 @@ extern "C" {
 using namespace std;
 
 int main() {
-	/*map<string, int> pokedex;
+	map<string, int> pokedex;
 
 	pokedex["bulbasaur"] = 1;
 	pokedex["pikachu"] = 25;
@@ -17,24 +17,8 @@ int main() {
 	pokedex["mightyena"] = 262;
 	pokedex["eevee"] = 133;
 
-	printf("%d elements;\n", pokedex.table().size());
-	for(const auto& elem : pokedex.table()) {
-		printf("=> %s\n", elem.first.c_str());
-	}
-
 	for(const auto& pair : pokedex) {
 		printf("%s is nr.%u\n", pair.first.c_str(), pair.second);
-	}*/
-
-	vector<string> pokemon;
-
-	pokemon.push_back("Scyther");
-	pokemon.push_back("Audino");
-	pokemon.push_back("Eevee");
-	pokemon.push_back("Chimecho");
-
-	for(const auto& p : pokemon) {
-		printf("%s\n", p.c_str());
 	}
 
 	return 0;
